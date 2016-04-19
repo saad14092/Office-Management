0931a11d7aa6938657418125934f4f69ac40e7ea
Saad Moumen <saad14092@gmail.com>
2016-04-19 09:26:09 +0000

    Just minor changes to classes we'll delete eventually.

--- src/org/mql/om/business/ToDelete.java
+++ src/org/mql/om/business/ToDelete.java
@@ -1,5 +1,13 @@
 package org.mql.om.business;
 
-public class ToDelete {
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
+public class ToDelete {
+	private static GregorianCalendar date = new GregorianCalendar();
+	
+	public static void main(String[] args) {
+		date.set(2014, 11, 11);
+		System.out.println(date.get(Calendar.DATE));
+	}
 }
